8b3cd5023aea155395083eb4f057271fcba42b89
Bruno Sousa <brunorsousa1998@gmail.com>
2021-10-01 00:31:38 -0300

    Criado cabeçalho da pagina

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="logo">Supera Games</h1>
+            <div className="cart">
+                <Link to="/carrinho">
+                    <img
+                        className="cart-icon"
+                        src={"assets/cart-icon.svg"}
+                        alt="cart"
+                    />
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
