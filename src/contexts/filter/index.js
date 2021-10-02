a360dfdaeb2f10ac1a8f05c1604bb8a5c3f86552
Bruno Sousa <brunorsousa1998@gmail.com>
2021-10-02 16:46:17 -0300

    Criado contexto de filtro de busca

---
+++ src/contexts/filter/index.js
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useReducer } from "react";
+import filterReducer from "./reducer";
+
+export const FilterContext = createContext();
+
+const FilterProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(filterReducer, {
+        searchString: "",
+        sortMethod: "none",
+        sortFunction: (products) => products,
+    });
+
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
+
+    const changeSearchString = (searchString) => {
+        dispatch({
+            type: "changeSearchString",
+            payload: { searchString },
+        });
+    };
+
+    const changeSortFunction = (sortMethod) => {
+        dispatch({
+            type: "changeSortFunction",
+            payload: { sortMethod },
+        });
+    };
+
+    return (
+        <FilterContext.Provider
+            value={{
+                searchString: state.searchString,
+                sortMethod: state.sortMethod,
+                sortFunction: state.sortFunction,
+                changeSearchString,
+                changeSortFunction,
+            }}
+        >
+            {children}
+        </FilterContext.Provider>
+    );
+};
+
+export default FilterProvider;
---
+++ src/contexts/filter/reducer.js
@@ -0,0 +1,51 @@
+const compareByName = (firstProduct, secondProduct) => {
+    if (firstProduct.name < secondProduct.name) return -1;
+    if (firstProduct.name > secondProduct.name) return 1;
+    return 0;
+};
+
+const compareByPrice = (firstProduct, secondProduct) => {
+    if (firstProduct.price < secondProduct.price) return -1;
+    if (firstProduct.price > secondProduct.price) return 1;
+    return 0;
+};
+
+const compareByScore = (firstProduct, secondProduct) => {
+    if (firstProduct.score < secondProduct.score) return -1;
+    if (firstProduct.score > secondProduct.score) return 1;
+    return 0;
+};
+
+const sortFunction = (sortMethod) => {
+    switch (sortMethod) {
+        case "name":
+            return (products) => products.sort(compareByName);
+        case "price":
+            return (products) => products.sort(compareByPrice);
+        case "score":
+            return (products) => products.sort(compareByScore);
+        default:
+            return (products) => products;
+    }
+};
+
+const filterReducer = (state, action) => {
+    switch (action.type) {
+        case "changeSearchString":
+            return {
+                searchString: action.payload.searchString,
+                sortMethod: state.sortMethod,
+                sortFunction: state.sortFunction,
+            };
+        case "changeSortFunction":
+            return {
+                searchString: state.searchString,
+                sortMethod: action.payload.sortMethod,
+                sortFunction: sortFunction(action.payload.sortMethod),
+            };
+        default:
+            return state;
+    }
+};
+
+export default filterReducer;
