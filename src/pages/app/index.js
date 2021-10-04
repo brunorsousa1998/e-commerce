3cb0776b02959d65d0bcf6d986f1dc4ee819e3d0
Bruno Sousa <brunorsousa1998@gmail.com>
2021-10-04 16:23:40 -0300

    Criado estilização para o app e ajustado rotas, contextos e componentes das paginas

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,9 +1,34 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import ProductProvider from "../../contexts/products";
+import CartProvider from "../../contexts/cart";
+
+import Header from "../../components/header";
+import Games from "../games";
+import Cart from "../cart";
+
+import "./style.css";
 
 const App = () => (
-    <ProductProvider>
-        <h1>Estrutura inicial do projeto</h1>
-    </ProductProvider>
+    <div className="app">
+        <Router>
+            <Header />
+            <ProductProvider>
+                <CartProvider>
+                    <section className="body">
+                        <section className="content">
+                            <Route path="/" exact>
+                                <Games />
+                            </Route>
+                            <Route path="/carrinho">
+                                <Cart />
+                            </Route>
+                        </section>
+                    </section>
+                </CartProvider>
+            </ProductProvider>
+        </Router>
+    </div>
 );
 
 export default App;
